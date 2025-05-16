7c71498cf827edf60812198c8ae8d9c7719580ed
creatlydev <creatlydev@gmail.com>
2025-05-16 04:11:16 +0000

    feat(Services): add services section and necessary resources

---
+++ src/consts/services.ts
@@ -0,0 +1,41 @@
+import brainwaveSymbolWhite from '../assets/brainwave-symbol-white.svg'
+import gradient from '../assets/gradient.png'
+import play from '../assets/play.svg'
+import ChatBubbleWing from '../assets/svg/ChatBubbleWing'
+import service1 from '../assets/services/service-1.png'
+import service2 from '../assets/services/service-2.png'
+import service3 from '../assets/services/service-3.png'
+import check from '../assets/check.svg'
+import chromeCast from '../assets/chrome-cast.svg'
+import recording01 from '../assets/recording-01.svg'
+import recording03 from '../assets/recording-03.svg'
+import disc02 from '../assets/disc-02.svg'
+import sliders04 from '../assets/sliders-04.svg'
+
+
+const brainwaveServices = [
+    "Photo generating",
+    "Photo enhance",
+    "Seamless Integration",
+];
+
+const brainwaveServicesIcons = [
+    recording03,
+    recording01,
+    disc02,
+    chromeCast,
+    sliders04,
+  ];
+
+export {
+    brainwaveSymbolWhite,
+    gradient,
+    play,
+    ChatBubbleWing,
+    service1,
+    service2,
+    service3,
+    check,
+    brainwaveServices,
+    brainwaveServicesIcons
+}
